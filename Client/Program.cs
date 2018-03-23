f10f6305e4bce7162733769b73f56a78c490724d
sbrl <sbrl@starbeamrainbowlabs.com>
2018-03-23 12:50:24 +0000

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+            Console.ReadKey();
+        }
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("http://localhost:54369/");
+            try
+            {
+                Task<string> task = GetStringAsync("/api/Translate/hello");
+                if (await Task.WhenAny(task, Task.Delay(20000)) == task)
+                { Console.WriteLine(task.Result); }
+                else
+                { Console.WriteLine("Request Timed Out"); }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetBaseException().Message);
+            }
+        }
+        static async Task<string> GetStringAsync(string path)
+        {
+            string responsestring = "";
+            HttpResponseMessage response = await client.GetAsync(path);
+            responsestring = await response.Content.ReadAsStringAsync();
+            return responsestring;
+        }
+    }
+}
---
+++ Lab5/Controllers/TranslateController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lab5.Controllers
+{
+    public class TranslateController : ApiController
+    {
+        // GET: api/Translate
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [Route("api/Translate/{input}")]
+        public string Get(string input)
+        {
+            return Request.CreateResponse().ToString();
+        }
+        [HttpGet]
+        [Route("api/Translate/GetId")]
+        public string GetId([FromUri]int id)
+        {
+            return "Your id is: " + id;
+        }
+
+        // GET: api/Translate/5
+        [HttpGet]
+        [Route("api/Translate/GetNumber")]
+        public string GetNumber([FromUri]int number)
+        {
+            number += 100;
+            return "Your number plus 100 is " + number.ToString();
+        }
+
+        // POST: api/Translate
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Translate/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Translate/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
